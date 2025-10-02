a3353d2cfb55d8cadd60e2f175427c30d6b34ed6
enusiraw <edlsiraw@gmail.com>
2025-10-03 00:13:25 +0300

    chore: update dependencies and app configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'bloc/food_menu/food_menu_bloc.dart';
 import 'bloc/cart/cart_bloc.dart';
 import 'bloc/cart/cart_event.dart';
